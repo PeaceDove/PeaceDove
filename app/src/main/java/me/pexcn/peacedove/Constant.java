7f00dc88be56837160dc85439fd0dbdbaffff5ae
pexcn <pexcn97@gmail.com>
2016-11-04 09:51:12 +0000

    Temp update

---
+++ app/src/main/java/me/pexcn/peacedove/Constant.java
@@ -0,0 +1,7 @@
+package me.pexcn.peacedove;
+
+/**
+ * Created by pexcn on 2016-06-29.
+ */
+public interface Constant {
+}
--- app/src/main/java/me/pexcn/peacedove/main/ui/MainActivity.java
+++ app/src/main/java/me/pexcn/peacedove/main/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package me.pexcn.peacedove;
+package me.pexcn.peacedove.main.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import me.pexcn.peacedove.R;
 
 public class MainActivity extends AppCompatActivity {
 
